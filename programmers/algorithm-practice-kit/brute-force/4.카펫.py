39a170173dbd764cbca9fcd0cd072ce133c17d84
Jayten <jayten.42@gmail.com>
2020-03-18 15:30:43 +0900

    Uddate 4.카펫.py
    좋은지 안 좋은지 모르겠는데 쨋든 줄임 ㅎ

--- programmers/algorithm-practice-kit/brute-force/4.카펫.py
+++ programmers/algorithm-practice-kit/brute-force/4.카펫.py
@@ -1,6 +1,4 @@
 def solution(brown, red):
     wide = brown + red
-    pairs = [(i, int(wide / i))for i in range(1, wide + 1) if wide % i == 0 and i >= int(wide / i)]
-    for x, y in pairs:
-        if brown == 2*x + 2*(y-2) and red == (y-2) * (x - 2):
-            return [x, y]
+    answer = [(i, int(wide / i)) for i in range(wide, 0, -1) if wide % i == 0 and (brown == 2*i + 2*(int(wide / i) -2) and red == (int(wide / i)-2) * (i - 2)) and i >= int(wide / i)]
+    return list(answer[0])
