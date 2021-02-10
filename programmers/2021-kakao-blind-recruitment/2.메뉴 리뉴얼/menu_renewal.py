9f8cc89c0c650d6305d9e40a5f30e80096e1378d
jaejeon <jayten.42@gmail.com>
2021-02-10 17:36:38 +0900

    Add: menu_renewal.py

--- programmers/2021-kakao-blind-recruitment/2.메뉴 리뉴얼/menu_renewal.py
+++ programmers/2021-kakao-blind-recruitment/2.메뉴 리뉴얼/menu_renewal.py
@@ -1,3 +1,25 @@
+from typing import List
+from itertools import combinations
+from collections import Counter
+
+
+def generate_course(orders, n_courses) -> List[str]:
+    course = []
+    for n in n_courses:
+        candidate = [
+            "".join(sorted(list(c))) for order in orders for c in combinations(order, n)
+        ]
+        counter = Counter(candidate)
+        if not counter:
+            continue
+        _, top_count = counter.most_common(1)[0]
+        if top_count < 2:
+            continue
+        course += [k for k, v in counter.most_common() if v == top_count]
+
+    return sorted(course)
+
+
 def solution(orders, course):
-    answer = []
+    answer = generate_course(orders, course)
     return answer
