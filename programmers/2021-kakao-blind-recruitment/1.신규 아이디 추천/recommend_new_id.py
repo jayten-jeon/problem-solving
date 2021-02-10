65885b97de7b1f5779dbcee70d136bd29cf72d51
jaejeon <jayten.42@gmail.com>
2021-02-10 14:56:00 +0900

    Feat: implement is_valid_id function

--- programmers/2021-kakao-blind-recruitment/1.신규 아이디 추천/recommend_new_id.py
+++ programmers/2021-kakao-blind-recruitment/1.신규 아이디 추천/recommend_new_id.py
@@ -1,3 +1,6 @@
+import re
+
+
 def is_valid_id(input_id) -> bool:
     """Validate input id with regular expression.
 
@@ -7,10 +10,19 @@ def is_valid_id(input_id) -> bool:
     Returns:
         bool: whether input id satisfies the regular expression.
     """
+    regex = re.compile("^[a-z0-9-_]{1}([a-z0-9-_][.]{0,1}){1,13}[a-z0-9-_]{1}$")
+    if regex.match(input_id) is None:
+        return False
+
+    return True
 
 
 def solution(new_id):
     answer = ""
+    if is_valid_id(new_id):
+        answer = new_id
+        return answer
+
     return answer
 
 
