4549047e64796367913a4fff57145dd3bf2594f2
jaejeon <jayten.42@gmail.com>
2021-02-11 00:12:54 +0900

    Add: Implement search_rank

--- programmers/2021-kakao-blind-recruitment/3.순위검색/search_rank.py
+++ programmers/2021-kakao-blind-recruitment/3.순위검색/search_rank.py
@@ -1,27 +1,41 @@
 from typing import List
-
+from collections import defaultdict
+import bisect
 
 # TODO: 효율성 통과를 위해 그룹화 하는 코드 작성
 
+db = defaultdict(list)
 
-def generate_db(info) -> List:
-    return [i.split() for i in info]
+
+def generate_raw(info: List, idx: int, columns: List):
+    global db
+
+    if idx == len(info) - 1:
+        key = " and ".join(columns)
+        bisect.insort(db[key], int(info[-1]))
+        return
+
+    generate_raw(info, idx + 1, columns + [info[idx]])
+    generate_raw(info, idx + 1, columns + ["-"])
+
+
+def generate_db(infos: List):
+    global db
+
+    for info in infos:
+        generate_raw(info.split(), 0, [])
 
 
 def search(query: List, db: List) -> List:
-    score = int(query[-1])
-    candidate = [raw for raw in db if int(raw[-1]) >= score]
+    candidate = db[query[0]]
+    i = bisect.bisect_left(candidate, query[1])
 
-    for i, q in enumerate(query[:-1]):
-        if q == "-":
-            continue
-        candidate = [raw for raw in candidate if raw[i] == q]
-    return candidate
+    return len(candidate) - i
 
 
 def solution(info, query):
     answer = []
-    db = generate_db(info)
-    query = [q.replace("and", "").split() for q in query]
-    answer = [len(search(q, db)) for q in query]
+    generate_db(info)
+    query = [(" ".join(q.split()[:-1]), int(q.split()[-1])) for q in query]
+    answer = [search(q, db) for q in query]
     return answer
