f8a93616aa40d24368fa7d3fb45a57a9ddf24f0a
jaejeon <jayten.42@gmail.com>
2021-02-10 16:25:44 +0900

    Feat: delete is_valid_id function and implement recommned_new_id function

--- programmers/2021-kakao-blind-recruitment/1.신규 아이디 추천/recommend_new_id.py
+++ programmers/2021-kakao-blind-recruitment/1.신규 아이디 추천/recommend_new_id.py
@@ -1,30 +1,30 @@
 import re
 
 
-def is_valid_id(input_id) -> bool:
-    """Validate input id with regular expression.
-
-    Args:
-        input_id (str): id that input from user.
-
-    Returns:
-        bool: whether input id satisfies the regular expression.
-    """
-    regex = re.compile("^[a-z0-9-_]{1}([a-z0-9-_][.]{0,1}){1,13}[a-z0-9-_]{1}$")
-    if regex.match(input_id) is None:
-        return False
-
-    return True
+def recommend_new_id(old_id: str):
+    new_id = ""
+    # 1단계 new_id의 모든 대문자를 대응되는 소문자로 치환합니다.
+    new_id = old_id.lower()
+    # 2단계 new_id에서 알파벳 소문자, 숫자, 빼기(-), 밑줄(_), 마침표(.)를 제외한 모든 문자를 제거합니다.
+    new_id = re.sub("[^a-z0-9-_.]", "", new_id)
+    # 3단계 new_id에서 마침표(.)가 2번 이상 연속된 부분을 하나의 마침표(.)로 치환합니다.
+    new_id = re.sub("[.]{2,}", ".", new_id)
+    # 4단계 new_id에서 마침표(.)가 처음이나 끝에 위치한다면 제거합니다.
+    new_id = new_id.strip(".")
+    # 5단계 new_id가 빈 문자열이라면, new_id에 "a"를 대입합니다.
+    if not new_id:
+        new_id = "a"
+    # 6단계 new_id의 길이가 16자 이상이면, new_id의 첫 15개의 문자를 제외한 나머지 문자들을 모두 제거합니다.
+    new_id = new_id[:15]
+    #     만약 제거 후 마침표(.)가 new_id의 끝에 위치한다면 끝에 위치한 마침표(.) 문자를 제거합니다.
+    new_id = new_id.rstrip(".")
+    # 7단계 new_id의 길이가 2자 이하라면, new_id의 마지막 문자를 new_id의 길이가 3이 될 때까지 반복해서 끝에 붙입니다.
+    if len(new_id) <= 2:
+        new_id += new_id[-1] * (3 - len(new_id))
+    return new_id
 
 
 def solution(new_id):
-    answer = ""
-    if is_valid_id(new_id):
-        answer = new_id
-        return answer
 
+    answer = recommend_new_id(new_id)
     return answer
-
-
-if __name__ == "__main__":
-    pass
