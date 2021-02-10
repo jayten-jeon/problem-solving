22f16c565ac916d972211a52061d6a9f1f1695f5
jaejeon <jayten.42@gmail.com>
2021-02-10 16:44:56 +0900

    Feat: Updated test.py

--- programmers/2021-kakao-blind-recruitment/2.메뉴 리뉴얼/test.py
+++ programmers/2021-kakao-blind-recruitment/2.메뉴 리뉴얼/test.py
@@ -10,16 +10,20 @@ class Test(unittest.TestCase):
         result = solution(orders, course)
         self.assertListEqual(expected, result)
 
-    def test_input_1(self):
+    def test_input_2(self):
         orders = ["ABCDE", "AB", "CD", "ADE", "XYZ", "XYZ", "ACD"]
         course = [2, 3, 5]
         expected = ["ACD", "AD", "ADE", "CD", "XYZ"]
         result = solution(orders, course)
         self.assertListEqual(expected, result)
 
-    def test_input_1(self):
+    def test_input_3(self):
         orders = ["XYZ", "XWY", "WXA"]
         course = [2, 3, 4]
         expected = ["WX", "XY"]
         result = solution(orders, course)
         self.assertListEqual(expected, result)
+
+
+if __name__ == "__main__":
+    unittest.main()
