bd35cca2e3010257648129304f62bae9ba7851e7
jaejeon <jayten.42@gmail.com>
2021-02-10 14:03:18 +0900

    Chore: move recommend_new_id to subdirectory

---
+++ programmers/2021-kakao-blind-recruitment/1.신규 아이디 추천/recommend_new_id.py
@@ -0,0 +1,18 @@
+def is_valid_id(input_id) -> bool:
+    """Validate input id with regular expression.
+
+    Args:
+        input_id (str): id that input from user.
+
+    Returns:
+        bool: whether input id satisfies the regular expression.
+    """
+
+
+def solution(new_id):
+    answer = ""
+    return answer
+
+
+if __name__ == "__main__":
+    pass
